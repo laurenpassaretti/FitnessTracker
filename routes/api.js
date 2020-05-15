0a63845bb3dd03953d87b51d110faf84a0095e37
Lauren Doss <doss.laurene@gmail.com>
2020-05-14 18:31:10 -0700

    updated so stats.html renders on page

--- routes/api.js
+++ routes/api.js
@@ -27,6 +27,10 @@ router.put("/api/workouts/:id", (req,res) => {
     
 })
 
+router.get("/stats", (req,res)=> {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+})
+
 router.get("/exercise", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html")); 
 }); 
